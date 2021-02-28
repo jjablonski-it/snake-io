de18a4fd9ebcddba878b8d7b889abb9e578dbcf9
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-02-28 15:50:20 +0100

    fixed dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,11 @@
+const NodemonPlugin = require("nodemon-webpack-plugin");
 const { merge } = require("webpack-merge");
 const config = require("./webpack.common");
 
 module.exports = merge(config, {
   mode: "development",
-  rules: {
-    module: [
+  module: {
+    rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
