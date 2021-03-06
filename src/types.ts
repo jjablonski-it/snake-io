d7cd834b03fb71cc5727bd4eb8b5f4c9a3563dfd
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-03-06 19:57:52 +0100

    removed redundant types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Snake } from "./utils/snake";
+
 export enum Direction {
   Up = 1,
   Right,
@@ -9,33 +11,15 @@ export enum TurnDirection {
   Left,
   Right,
 }
-
-type Optional<T> = {
-  [K in keyof T]?: T[K];
-};
-
 export interface Vector {
   x: number;
   y: number;
 }
 
-export interface Snake {
-  head: Vector;
-  segments: Vector[];
-  length: number;
-  direction: Direction;
-  forward: () => void;
-  turn: (props: TurnDirection) => void;
-  checkCollision: () => boolean;
-}
-
-export type OptionalSnake = Optional<Snake>;
-
 export interface Player {
   id: string;
   snake: Snake;
 }
-
 export interface State {
   players: Player[];
   fruit: Vector;
