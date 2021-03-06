439dbd81ff9fbf6903a8f9b9558e959de0c1f27e
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-03-06 17:56:09 +0100

    added drawing snake head to canvas

--- src/client/script.ts
+++ src/client/script.ts
@@ -1,11 +1,16 @@
 import io from "socket.io-client";
 import { State, TurnDirection } from "../types";
+import { SCALE } from "../utils/constants";
 import "./style.css";
 
 let socket = io();
+const canvas = document.querySelector("canvas");
+const ctx = canvas?.getContext("2d");
+ctx?.scale(...SCALE);
 
 socket.on("update", (data: State) => {
   console.log(data);
+  if (ctx) draw(data, ctx);
 });
 
 window.addEventListener("keydown", (e) => {
@@ -15,3 +20,13 @@ window.addEventListener("keydown", (e) => {
     socket.emit("turn", TurnDirection.Right);
   }
 });
+
+const draw = (data: State, ctx: CanvasRenderingContext2D) => {
+  data.players.forEach((p) => {
+    const { snake } = p;
+    const {
+      head: { x, y },
+    } = snake;
+    ctx.fillRect(x, y, 1, 1);
+  });
+};
