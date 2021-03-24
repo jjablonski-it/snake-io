c6ddd7eb9a5843a3475b70445243bdb58fd54a56
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-03-24 21:48:45 +0100

    added drawGrid function

--- src/client/utils/canvas.ts
+++ src/client/utils/canvas.ts
@@ -1,5 +1,5 @@
 import { StateDTO } from "../../types";
-import { SCALE } from "../../utils/constants";
+import { GRID_P, SCALE } from "../../utils/constants";
 
 export const createCanvasController = (ctx: CanvasRenderingContext2D) => {
   const { width, height } = ctx.canvas;
@@ -13,11 +13,29 @@ export const createCanvasController = (ctx: CanvasRenderingContext2D) => {
     ctx.scale(SCALE, SCALE);
 
     drawBorders(data);
+    drawGrid();
   };
 
   const drawBorders = ({ worldSize }: StateDTO) => {
+    ctx.lineWidth = 0.1;
     ctx.clearRect(0, 0, width, height);
     ctx.strokeRect(0, 0, worldSize, worldSize);
+  };
+
+  const drawGrid = () => {
+    ctx.lineWidth = 0.01;
+    ctx.beginPath();
+
+    for (let x = 0; x < width; x += GRID_P) {
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+    }
+
+    for (let y = 0; y < height; y += GRID_P) {
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+    }
+    ctx.stroke();
   };
 
   return { update, setSize };
